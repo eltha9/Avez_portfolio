6ad38cc661859838ab964c5063daf58c153cf9fd
eltha9 <theadosto@gmail.com>
2019-05-18 19:45:00 +0200

    add first screen

--- index.php
+++ index.php
@@ -4,10 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gaëtan Avez - French Designer </title>
-    <link rel="stylesheet" href="./styles/style.css">
+    <title>Gaëtan Avez - French Designer</title>
+    <link rel="stylesheet" href="./styles/home.css">
 </head>
 <body>
+    <div class="home-animation-window">
+        <div class="window-side"></div>
+        <div class="window-center"></div>
+        <div class="window-center"></div>
+        <div class="window-center"></div>
+        <div class="window-center"></div>
+        <div class="window-center"></div>
+        <div class="window-side right"></div>
+    </div>
+    <a class="logo"href="#">gaetan avez.</a>
+    <div class="social">
+        <a href="#"><img src="./images/linkedin.svg" alt=""></a>
+        <a class="facebook" href="#"><img src="./images/facebook.svg" alt=""></a>
+        <a href="#"><img src="./images/dribble.svg" alt=""></a>
+    </div>
     
+    <div class="window-text">
+        <span>Welcome to my</span>
+        <span>portfolio</span>
+    </div>
+    <script src="./scripts/app.js"></script>
 </body>
 </html>
