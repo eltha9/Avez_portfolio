30cd3beb6f398e92757cb669c617206dd1a7b9a9
eltha9 <theadosto@gmail.com>
2019-06-09 19:35:13 +0200

    finish about

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gaëtan Avez - French Designer</title>
-    <link rel="stylesheet" href="./styles/contact.css">
+    <link rel="stylesheet" href="./styles/about.css">
 </head>
 <body>
     <div class="home-animation-window">
@@ -13,7 +13,7 @@
         <div class="window-center border"></div>
         <div class="window-center border"></div>
         <div class="window-center border">
-            <a class="nav" href="project">About me</a>
+            <a class="nav" href="about">About me</a>
         </div>
         <div class="window-center border">
             <a class="nav" href="home">Project</a>
@@ -32,8 +32,87 @@
 
     </div>
     <main>
+        <div class="top">
+            <div class="left">
+                <div class="contouring">
 
+                </div>
+                <div class="bg">
+                    <h3>ME.</h3>
+                    <img src="images/projects/ME.jpg" alt="">
+                </div>
+            </div>
+            <div class="right">
+                <h1>Hello, my name is Gaëtan Avez, i’m a french designer based in Paris.</h1>
+                <a href="AVEZ_CV.pdf" target="blanck">My resume <img src="images/download_button.svg" alt=""></a>
+            </div>
+        </div>
+        <div class="middle">
+            <h3>Experience</h3>
+            <div class="exp">
+                <div class="project">
+                    <div class="left">
+                        <h4>Designer</h4>
+                        <span>UI-UX</span>
+                    </div>
+                    <div class="mid">
+                        <h4>Thibaultrichard.fr</h4>
+                        <span>Portfolio</span>
+                    </div>
+                    <div class="right">
+                        <h4>2019</h4>
+                    </div>
+                </div>
+                <div class="project">
+                    <div class="left">
+                        <h4>Designer</h4>
+                        <span>UI-UX</span>
+                    </div>
+                    <div class="mid">
+                        <h4>UniColor</h4>
+                        <span>School Project</span>
+                    </div>
+                    <div class="right">
+                        <h4>2019</h4>
+                    </div>
+                </div>
+            </div>
+            <img src="images/about_down_button.svg" alt="">
+        </div>
+        <div class="bottom">
+            <div class="language">
+                <h4>Languages</h4>
+                <div class="content">
+                    <div class="idioma">
+                        <span>French</span>
+                        <span>English</span>
+                        <span>Spanish</span>
+                    </div>
+                    <div class="level">
+                        <span>Native</span>
+                        <span>Fluent</span>
+                        <span>Elementary</span>
+                    </div>
+                </div>
+            </div>
+            <div class="skills">
+                <h4>Skills</h4>
+                <div class="content">
+                    <div>
+                        <h5>Design</h5>
+                        <span>Sketch, Principle, Photoshop, Illustrator</span>
+                    </div>
+                    <div>
+                        <h5>Web developpment</h5>
+                        <span>HTML 5, CSS3, JavaScript, PHP</span>
+                    </div>
+                    <div>
+                        <h5>Photography</h5>
+                        <span>Lightroom</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
-    <script src="./scripts/app.js"></script>
 </body>
 </html>
